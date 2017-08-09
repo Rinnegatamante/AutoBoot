18bfc7ac132b3f56b9bc9ec345776a9c9acdf7b7
Rinnegatamante <rinnegatamante@gmail.com>
2017-08-09 13:22:46 +0200

    Made it compatible with enso.
    Made it compatible with enso.

--- main.c
+++ main.c
@@ -11,17 +11,20 @@ int launchAppByTitleid(const char *titleid) {
 	char uri[32];
 	sprintf(uri, "psgm:play?titleid=%s", titleid);
 	
-	int i;
-	for (i=0;i<400;i++){
+	do{
 		sceKernelDelayThread(10000);
-		sceAppMgrLaunchAppByUri(0xFFFFF, uri);
-	}
+		int ret = sceAppMgrLaunchAppByUri(0xFFFFF, uri);
+		if (ret == 0) break;
+	}while(1);
 
 	return 0;
 }
 
-void _start() __attribute__ ((weak, alias ("module_start")));
-int module_start(SceSize argc, const void *args) {
+int autoboot_thread(SceSize args, void *argp){
+	
+	// Waiting a bit to let enso setup
+	sceKernelDelayThread(10000000);
+	
 	SceUID fd = sceIoOpen("ux0:/data/AutoBoot/boot.cfg", SCE_O_RDONLY, 0777);
 	if (fd >= 0){
 		sceIoRead(fd, titleid, 9);
@@ -30,6 +33,17 @@ int module_start(SceSize argc, const void *args) {
 		launchAppByTitleid(titleid);
 	}
 	
+	return 0;
+	
+}
+
+void _start() __attribute__ ((weak, alias ("module_start")));
+int module_start(SceSize argc, const void *args) {
+	
+	// Starting secondary thread to delay boot until enso is setup
+	SceUID thid = sceKernelCreateThread("autoboot_thread", autoboot_thread, 0x40, 0x100000, 0, 0, NULL);
+	if (thid >= 0) sceKernelStartThread(thid, 0, NULL);
+
 	return SCE_KERNEL_START_SUCCESS;
 
 }
